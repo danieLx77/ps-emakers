ea8b32d29553d61f80b822dacf3ae134cb68e1d3
Daniel de Jesus Moreira <danieljesus7k@gmail.com>
2026-04-30 11:43:20 -0300

    feat: configura estrutura inicial do spring boot

---
+++ src/main/java/br/com/emakers/psemakers/PsEmakersApplication.java
@@ -0,0 +1,13 @@
+package br.com.emakers.psemakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsEmakersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PsEmakersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/emakers/psemakers/PsEmakersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.emakers.psemakers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsEmakersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
