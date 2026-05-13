3343651e9d7eba0753577b762ff26b3a7f3168b5
Daniel de Jesus Moreira <danieljesus7k@gmail.com>
2026-05-13 00:16:42 -0300

    config: habilita o uso de FeignClients na aplicacao

--- src/main/java/br/com/emakers/psemakers/PsEmakersApplication.java
+++ src/main/java/br/com/emakers/psemakers/PsEmakersApplication.java
@@ -2,8 +2,10 @@ package br.com.emakers.psemakers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PsEmakersApplication {
 
     public static void main(String[] args) {
