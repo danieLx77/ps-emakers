baeb50f8b9e55a80abccc41f6bd31247d3f2761a
Daniel de Jesus Moreira <danieljesus7k@gmail.com>
2026-05-13 00:22:19 -0300

    chore: remove comentarios obsoletos

--- src/main/java/br/com/emakers/psemakers/data/dto/response/EnderecoResponse.java
+++ src/main/java/br/com/emakers/psemakers/data/dto/response/EnderecoResponse.java
@@ -4,6 +4,6 @@ public record EnderecoResponse(
         String cep,
         String logradouro,
         String bairro,
-        String localidade, // O ViaCEP usa 'localidade' para o nome da cidade
+        String localidade,
         String uf
 ) {}
