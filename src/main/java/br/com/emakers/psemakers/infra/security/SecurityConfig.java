0c355eb9eb2f90aae65d73ead050ecee2aab9690
Daniel de Jesus Moreira <danieljesus7k@gmail.com>
2026-05-28 19:36:00 -0300

    config: adiciona openapiconfig com suporte a jwt e libera rotas no security

--- src/main/java/br/com/emakers/psemakers/infra/security/SecurityConfig.java
+++ src/main/java/br/com/emakers/psemakers/infra/security/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/pessoas").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
